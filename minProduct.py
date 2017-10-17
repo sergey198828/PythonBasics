9b99ccb2a0d2f6120c1cc807ff8f10c6d2e93aa5
Sergey Kiyan <sergey198828@mail.ru>
2017-10-17 12:27:47 +0300

    Initial Commit

---
+++ minProduct.py
@@ -0,0 +1,87 @@
+import unittest
+
+
+# Unit test cases
+class MinProductTestCase(unittest.TestCase):
+    def test_void_string(self):
+        self.assertRaises(ValueError, min_product, [])
+
+    def test_one_string(self):
+        self.assertRaises(ValueError, min_product, [1])
+
+    def test_two_string(self):
+        expected_output = -1
+        self.assertEqual(expected_output, min_product([1, -1]))
+
+    def test_positive_string_no_zero(self):
+        expected_output = 2
+        self.assertEqual(expected_output, min_product([3, 3, 1, 2, 2]))
+
+    def test_positive_string_with_zero(self):
+        expected_output = 0
+        self.assertEqual(expected_output, min_product([3, 1, 2, 0]))
+
+    def test_negative_string_no_zero(self):
+        expected_output = 1
+        self.assertEqual(expected_output, min_product([-3, -1, -1, -2]))
+
+    def test_positive_string_with_zero(self):
+        expected_output = 0
+        self.assertEqual(expected_output, min_product([-3, -1, -2, 0]))
+
+    def test_general_string(self):
+        expected_output = -10
+        self.assertEqual(expected_output, min_product([10, 0, -1]))
+
+
+def min_product(arr):
+    """Takes array of real numbers
+    Returns minimal product of 2 elements
+    Raise an exception if not enough elements"""
+
+    # Special cases:
+    # Len(arr)<2 - undefined
+    if len(arr) < 2:
+        raise ValueError("Array length must be greater or equal to 2")
+    # Len(arr)=2 - arr[0]*arr[1]
+    if len(arr) == 2:
+        return arr[0] * arr[1]
+    # Loop once over array to find if zero presented as well as 2 maximum and 2 minimum elements (initialize with
+    # first 2)
+    has_zero = True if (arr[0] == 0 or arr[1] == 0) else False
+    if arr[0] >= arr[1]:
+        min1, min2, max1, max2 = arr[1], arr[0], arr[0], arr[1]
+    else:
+        min1, min2, max1, max2 = arr[0], arr[1], arr[1], arr[0]
+    for i in arr[2:]:
+        if i == 0 and not has_zero:
+            has_zero = True
+        if i < min1:
+            min2, min1 = min1, i
+            continue
+        if i < min2:
+            min2 = i
+        if i > max1:
+            max2, max1 = max1, i
+            continue
+        if i > max2:
+            max2 = i
+    # Regular cases:
+    # Arr has positives and negatives - min(arr)*max(arr)
+    if min1 < 0 < max1:
+        return min1 * max1
+    # Arr has only positives without zero min1(arr)*min2(arr)
+    if not has_zero:
+        if min1 > 0:
+            return min1*min2
+    # Arr has only negatives without zero max1(arr)*max2(arr)
+        else:
+            return max1 * max2
+    # Arr has only positives and zero or only negatives and zero - 0
+    return 0
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
---
+++ validateLogin.py
@@ -0,0 +1,54 @@
+import re
+import unittest
+
+
+# Unit test cases
+class ValidateLoginTestCase(unittest.TestCase):
+    def test_void_login(self):
+        expected_output = ['Login length must be between 1 and 20 symbols', 'Login must start from letter', 'Login must \
+end by letter or digit']
+        self.assertEqual(expected_output, login_validation(""))
+
+    def test_one_invalid_character(self):
+        expected_output = ['Only letters, digits, minus and dot are permitted', 'Login must start from letter', 'Login \
+must end by letter or digit']
+        self.assertEqual(expected_output, login_validation("&"))
+
+    def test_start_with_digit(self):
+        expected_output = ['Login must start from letter']
+        self.assertEqual(expected_output, login_validation("1-.1"))
+
+    def test_all_good(self):
+        expected_output = []
+        self.assertEqual(login_validation("a-1"), expected_output)
+        self.assertEqual(login_validation("a.a"), expected_output)
+
+    def test_start_with_digit_invalid_character_inside_end_by_invalid_character(self):
+        expected_output = ['Only letters, digits, minus and dot are permitted', 'Login must start from letter', 'Login \
+must end by letter or digit']
+        self.assertEqual(login_validation("1a.+9$"), expected_output)
+
+
+def login_validation(login):
+    """Function
+    Takes login as string
+    Returns result of compliance validation as array of strings, void array means pass"""
+
+    result = []
+    # Check for length
+    if len(login) < 1 or len(login) > 20:
+        result.append("Login length must be between 1 and 20 symbols")
+    # Check if has only appropriate letters
+    if not re.match(r"^[A-Za-z0-9.-]*$", login):
+        result.append("Only letters, digits, minus and dot are permitted")
+    # Check first character
+    if not re.match(r"^[A-Za-z]", login):
+        result.append("Login must start from letter")
+    # Check last character
+    if not re.match(r".*[A-Za-z0-9]$", login):
+        result.append("Login must end by letter or digit")
+    return result
+
+
+if __name__ == '__main__':
+    unittest.main()
